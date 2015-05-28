5ad1ad982be6d254d8017f4fa3796e4a2d541930
Owain Lewis <owain@owainlewis.com>
2015-05-28 21:04:59 +0100

    finish later

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
+import           Control.Arrow
 import           Control.Monad.State
 
 -- Haskell Stack DataStructure
---
 
 type Stack a = [a]
 
@@ -13,10 +13,24 @@ create = []
 push :: a -> Stack a -> Stack a
 push = (:)
 
-pop :: Stack a -> (a, Stack a)
-pop []     = error "Stack empty"
-pop (x:xs) = (x,xs)
+pop :: Stack a -> Stack a
+pop []     = []
+pop (x:xs) = xs
 
 peek :: Stack a -> Maybe a
 peek []     = Nothing
 peek (x:xs) = Just x
+
+runBinaryOp stack op =
+  let a = pop stack
+      b = pop stack in
+  push (a `op` b)
+
+data AST =
+    Command String
+  | Atom String
+  deriving ( Show, Eq )
+
+data VirtualMachine = VirtualMachine {
+  initialStack :: Stack AST
+}
