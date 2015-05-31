7e5370860bbbf8d2eadea01826679af8e33cc3a4
Owain Lewis <owain@owainlewis.com>
2015-05-31 21:17:29 +0100

    finish for today

--- src/Language/Scratch.hs
+++ src/Language/Scratch.hs
@@ -1,10 +1,14 @@
 module Language.Scratch where
 
+import           Control.Monad.State
+import qualified Data.Map            as M
+
 data Command =
     Push Int
   | Add
   | Mult
   | Dup
+  | Swap
   | Dot
   | Subtract
   deriving ( Show )
@@ -12,14 +16,26 @@ data Command =
 type Program = [Command]
 type Stack   = [Int]
 
+popS :: State Stack Int
+popS = state $ \(x:xs) -> (x,xs)
+
+pushS :: Int -> State Stack ()
+pushS y = state $ \xs -> ((), y:xs)
+
+binaryOperation op = do
+    x <- get
+    y <- get
+    put (x `op` y)
+
 parseProgram :: String -> [Command]
-parseProgram = map asCommand . words
+parseProgram = (map asCommand) . words
     where
-        asCommand "."   = Dot
-        asCommand "+"   = Add
-        asCommand "*"   = Mult
-        asCommand "DUP" = Dup
-        asCommand x     = Push (read x)
+        asCommand "."    = Dot
+        asCommand "+"    = Add
+        asCommand "*"    = Mult
+        asCommand "DUP"  = Dup
+        asCommand "SWAP" = Swap
+        asCommand x      = Push (read x)
 
 runProgram :: Program -> Stack -> Stack
 runProgram [] stack = stack
@@ -27,7 +43,20 @@ runProgram (x:xs) stack = runProgram xs (runCommand x stack)
 
 runCommand :: Command -> Stack -> Stack
 runCommand (Push n) stack = n : stack
-runCommand Dup (x:xs) = x:x:xs
-runCommand Add (x:y:xs) = x+y:xs
+runCommand Dup (x:xs)     = x:x:xs
+runCommand Add (x:y:xs)   = x+y:xs
+runCommand _ _ = error "empty stack"
 
-forth program = (\p -> runProgram p []) . parseProgram
+execute :: String -> Stack -> Stack
+execute program stack = runProgram parsed stack
+  where parsed = parseProgram program
+
+loop :: Stack -> IO String
+loop stack = do
+    putStrLn "Welcome to FORTH"
+    putStrLn ">>>"
+    putStrLn (show stack)
+    code   <- getLine
+    let result = execute code stack
+    putStrLn . show $ result
+    loop result
